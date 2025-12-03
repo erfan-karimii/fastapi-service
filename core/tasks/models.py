db302587d87279f577e934ec474ea816e134f992
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2025-12-03 10:33:04 +0330

    Add user relation to task model and update migration scripts

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -1,14 +1,16 @@
 from core.base_model import BaseModel 
-from sqlalchemy import  String, Boolean , Text 
+from sqlalchemy import  String, Boolean , Text , ForeignKey 
 from sqlalchemy.orm import Mapped , mapped_column
 
 
 class TaskModel(BaseModel):
     __tablename__ = "tasks"
 
+    user: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     title:Mapped[str] = mapped_column(String(256), nullable=False)
     description:Mapped[str] = mapped_column(Text, nullable=True)
     is_completed:Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+
 
     def __repr__(self) -> str:
         return f"Task(id={self.id}, title={self.title}, is_completed={self.is_completed})"

4922d0b0d12ab9f96710168a25563969051d5d42
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2025-12-05 17:35:18 +0330

    Add user and task seeding functionality with Faker and update requirements

---
+++ core/datagen.py
@@ -0,0 +1,47 @@
+from core.database import SessionLocal
+from sqlalchemy.orm import Session
+from users.models import UserModel
+from tasks.models import TaskModel
+from faker import Faker
+
+
+fake = Faker()
+
+
+def seed_users(db:Session) -> UserModel:
+    user = UserModel(username=fake.user_name())
+    user.set_password("12345678")
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    print(f"User created with Username: {user.username} and ID: {user.id}")
+    return user
+
+
+def seed_tasks(db:Session, user:UserModel, count:int=10) -> None:
+    tasks_list = []
+    for _ in range(count):
+        tasks_list.append(
+            TaskModel(
+                user_id=user.id,
+                title=fake.sentence(nb_words=6),
+                description=fake.text(),
+                is_completed=fake.boolean(),
+            )
+        )
+    db.add_all(tasks_list)
+    db.commit()
+    print(f"added 10 tasks for user id {user.id}")
+
+
+def main() -> None:
+    db = SessionLocal()
+    try:
+        user = seed_users(db)
+        seed_tasks(db, user)
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    main()
